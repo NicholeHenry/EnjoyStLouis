f034658e8c2933cc90bd1ec3d7b64dca19d52721
Nichole Henry <nichole.henry12@gmail.com>
2019-06-26 16:18:12 -0500

    commit

--- EnjoyStLouis/Models/RateReview.cs
+++ EnjoyStLouis/Models/RateReview.cs
@@ -7,7 +7,10 @@ namespace EnjoyStLouis.Models
 {
     public class RateReview : IModel
     {
-       public int Id { get; set; }
+        
+        
+        public int Id { get; set; }
+        public string BuisnessName { get; set; }
         public int Rating { get; set; }
         public string Review { get; set; }
         public int BuisnessId { get; set; }
---
+++ EnjoyStLouis/ViewModels/RateReview/RateReviewCreateViewModel.cs
@@ -0,0 +1,55 @@
+﻿using EnjoyStLouis.Data.Repositories;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EnjoyStLouis.Data;
+using EnjoyStLouis.Models;
+
+namespace EnjoyStLouis.ViewModels.RateReview
+{
+    public class RateReviewCreateViewModel
+    {
+        private string ratings = "12345";
+
+        private readonly Factory repositoryFactory;
+
+        public int Id { set; get; }
+        public string BuisnessName { get; set; }
+        public string Review { get; set; }
+        public int Rating { get; set; }
+        public int BuisnessId { get; set; }
+
+        public SelectList Ratings { get { return GetRatings(); } }
+
+        public RateReviewCreateViewModel(Factory repositoryFactory)
+        {
+            this.repositoryFactory = repositoryFactory;
+        }
+       
+       private SelectList GetRatings()
+        {
+            var rateSelectedItem = ratings.Select(r => new SelectListItem(r.ToString(), r.ToString()));
+            return new SelectList(rateSelectedItem);
+        }
+
+        private string GetReviews()
+
+        internal void Persist()
+        {
+            Models.RateReview rateReview = new Models.RateReview
+            {
+
+                BuisnessId = this.BuisnessId,
+                Rating = this.Rating
+                Review = this.Review
+                BuisnessName = Buisness.Name
+            };
+
+            repositoryFactory.GetRateReviewRepository().Save(rateReview);
+        }
+
+    }
+}
+ 
